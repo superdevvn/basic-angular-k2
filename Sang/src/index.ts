9d4786052f5d42b787b538bca630aa98a490a01c
hrtlkr29 <kevinsandjose29@gmail.com>
2018-01-24 12:01:56 +0700

    a

---
+++ Sang/src/index.ts
@@ -0,0 +1,38 @@
+
+import * as $ from 'jquery';
+
+$(document).ready(function () {
+	let func = function () {
+
+		$("#firstname").keyup(func);
+		$("#lastname").keyup(func);
+
+		$("#btnHello").click(() => {
+			let fullName = $("#fullname");
+			let current = fullName.prop('disabled', )
+			fullName.prop('disabled', !current);
+		});
+
+		$("#btnHello").mouseenter(() => {
+			let btn1 = $("#btnHello");
+			btn1.addClass('btn-primary');
+			// btn1.css('background','red', 'important');
+		});
+
+		$("#btnHello").mouseleave(() => {
+			let btn1 = $("#btnHello");
+			// btn1.css('background','yellow', 'important');
+			btn1.removeClass('btn-primary');
+		});
+
+		$("btnHide").dblclick(()=>{
+			let btn1 = $("#btnHello");
+			if(btn1.is(":visible"))
+			{
+			btn1.hide();
+			}
+		}
+	)}
+
+})
+
