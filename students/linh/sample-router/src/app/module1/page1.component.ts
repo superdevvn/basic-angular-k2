42e89151d631f41ce0629f4ed4806a391371da5d
nnlinh97 <nnlinh97it@gmail.com>
2018-01-29 19:02:07 +0700

    commit sample-router

---
+++ students/linh/sample-router/src/app/module1/page1.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+// class Teacher {
+//     Name: string;
+//     Age: number;
+//     Salary: number;
+// }
+@Component({
+    selector: 'page1',
+    templateUrl: "./page1.component.html",
+    styleUrls: ['./page1.component.css']
+})
+export class Page1Component {
+    // ArrTeachers: Teacher[] = [];
+    // constructor() { }
+    // ngOnInit() {
+    //     let teacher = new Teacher();
+    //     teacher.Name = "Nguyen Van A";
+    //     teacher.Age = 23;
+    //     teacher.Salary = 2000;
+    //     this.ArrTeachers.push(teacher);
+
+    //     teacher = new Teacher();
+    //     teacher.Name = "Nguyen Thi B";
+    //     teacher.Age = 36;
+    //     teacher.Salary = 1500;
+    //     this.ArrTeachers.push(teacher);
+
+    //     teacher = new Teacher();
+    //     teacher.Name = "Nguyen Ngoc C";
+    //     teacher.Age = 32;
+    //     teacher.Salary = 4000;
+    //     this.ArrTeachers.push(teacher);
+    // }
+}
---
+++ students/linh/sample-router/src/app/module2/page1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page2',
+  templateUrl: "./page1.component.html"
+
+})
+export class Page1Component {
+}
