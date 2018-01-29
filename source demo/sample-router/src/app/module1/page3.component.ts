c557e4acd9559bbed562a949aa2c5645e0113762
PCLap\Administrator <syquang229@gmail.com>
2018-01-29 17:39:34 +0700

    no message

---
+++ source demo/sample-router/src/app/module1/page3.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'page1',
+    template: `<h1 style="color:yellow">This is Module1's Page3</h1>`
+})
+export class Page3Component {
+}
---
+++ source demo/sample-router/src/app/module2/page2.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'page2',
+    template: `<h1 style="color:purple">This is Module2's Page2</h1>`
+})
+export class Page2Component {
+}
