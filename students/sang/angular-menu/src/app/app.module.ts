5eef185f671c53772621063bc32531232466ef2d
hrtlkr29 <kevinsandjose29@gmail.com>
2018-02-20 12:59:20 +0700

    sang

--- students/sang/angular-menu/src/app/app.module.ts
+++ students/sang/angular-menu/src/app/app.module.ts
@@ -1,25 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
+import { HttpModule } from "@angular/http";
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { TacGiaComponent } from './tac-gia/tac-gia.component';
+import { NhanVienComponent } from './nhan-vien/nhan-vien.component';
+import { DocGiaComponent } from './doc-gia/doc-gia.component';
+import { SachComponent } from './sach/sach.component';
+import { MuonTraComponent } from './muon-tra/muon-tra.component';
 
 const routes: Routes =[
   {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: "main", component: MenuComponent, children:[
-    {path :'', redirectTo:"app-menu",pathMatch:"full"}
+    {path :'', redirectTo:"tac-gia",pathMatch:"full"},
+    {path : 'tac-gia', component: TacGiaComponent},
+    {path : 'nhan-vien', component: NhanVienComponent},
+    {path : 'doc-gia', component: DocGiaComponent},
+    {path : 'sach', component: SachComponent},
+    {path : 'muon-tra', component: MuonTraComponent},
   ]}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TacGiaComponent,
+    NhanVienComponent,
+    DocGiaComponent,
+    SachComponent,
+    MuonTraComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
