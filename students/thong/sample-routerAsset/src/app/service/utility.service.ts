4be6e3f869c47c887aeb026351894f3c7f4e1c91
ThongLV <thonglevan131@gmail.com>
2018-02-26 21:04:07 +0700

    login su dung http service

--- students/thong/sample-routerAsset/src/app/service/utility.service.ts
+++ students/thong/sample-routerAsset/src/app/service/utility.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http/src/http';
 
 @Injectable()
 export class UtilityService {
+
+    constructor(private http:Http){}
+
     fullName:string;
     hello(firstName: string, lastName: string) {
         this.fullName = `${firstName} ${lastName}`
@@ -17,13 +21,20 @@ export class UtilityService {
         return promise;
     }
 
-    login(userName:string, password:string){
-        return new Promise((resolve, reject)=>{
-            setTimeout(()=>{
-                if (userName=="peter" && password=="123"){
-                    resolve();
-                } else reject("Sai ten dang nhap hoac mat khau");
-            },1000);
-        });
+    // login(userName:string, password:string){
+    //     return new Promise((resolve, reject)=>{
+    //         setTimeout(()=>{
+    //             if (userName=="peter" && password=="123"){
+    //                 resolve();
+    //             } else reject("Sai ten dang nhap hoac mat khau");
+    //         },1000);
+    //     });
+    // }
+    login(userName:string,password:string){
+        let url = "http://http://103.232.121.69:5103/api/login";
+        return this.http.post(url, {
+            userName: userName,
+            password: password
+        }).toPromise();
     }
 }
