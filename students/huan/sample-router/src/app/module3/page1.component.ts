33f4c16dadfa4f07bda87aa219373277cd69f3d3
tdhuan <dinhhuantran3010@gmail.com>
2018-02-26 20:50:53 +0700

    commit sample-jquery and sample-router

---
+++ students/huan/sample-router/src/app/module3/page1.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page1',
+  template: `<h1 style="color:red">This is Module3's Page1</h1>`
+})
+export class Page1Component {
+}
