78353b233b733985f41289de878a4594ae117035
Trần Thuận Nghĩa <tran.thuan.nghia@gmail.com>
2018-03-12 15:18:56 +0700

    Back-end for QLKS

---
+++ projects/quan-ly-ban-hang-02/Back-end/SuperDev.Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace SuperDev.Models
+{
+    public partial class SuperDevDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+    public class Customer
+    {
+
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual List<Order> Orders { get; set; }
+    }
+}
---
+++ projects/quan-ly-ban-hang-02/Back-end/SuperDev.Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using SuperDev.Models;
+using SuperDev.Repositories;
+
+namespace SuperDev.Services
+{
+    public class ProductService
+    {
+        public Product PersistProduct(Product product)
+        {
+            var productRepository = new ProductRepository();
+            if (product.Id > 0) return productRepository.Update(product);
+            return productRepository.Create(product);
+        }
+
+        public IEnumerable<ProductComplex> GetList()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.GetEntities();
+        }
+
+        public Product GetById(int id)
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.GetEntity(id);
+        }
+    }
+}
