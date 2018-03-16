843ceabfb51630aba67deaa5475090025def7b7a
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-03-16 19:56:52 +0700

    - Update Back-end QLBH

--- projects/quan-ly-ban-hang-01/Back-end/SuperDev.Services/UserService.cs
+++ projects/quan-ly-ban-hang-01/Back-end/SuperDev.Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using SuperDev.Models;
 using SuperDev.Repositories;
+using SuperDev.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Web;
@@ -8,17 +9,20 @@ namespace SuperDev.Services
 {
     public class UserService
     {
-        public User PersistUser(User user)
+        public User Persist(User user)
         {
             var userRepository = new UserRepository();
             if (user.Id > 0)
             {
                 var entity = userRepository.GetEntity(user.Id);
+                if (!string.Equals(entity.Password, user.Password)) user.Password = Utility.HashMD5(user.Password);
                 user.CreatedDate = entity.CreatedDate;
                 user.CreatedBy = entity.CreatedBy;
                 return userRepository.Update(user);
-            } else
+            }
+            else
             {
+                user.Password = Utility.HashMD5(user.Password);
                 user.CreatedDate = DateTime.Now;
                 user.CreatedBy = GetCurrentUser().Id;
                 return userRepository.Create(user);
@@ -40,21 +44,23 @@ namespace SuperDev.Services
         public User Login(string username, string password)
         {
             var userRepository = new UserRepository();
-            var user = userRepository.GetEntity(username, password);
+            var user = userRepository.GetEntity(username, Utility.HashMD5(password));
             if (user == null) throw new Exception("Sai tên đăng nhập hoặc mật khẩu");
             return user;
         }
 
         public string Encrypt(User user)
         {
-            return user.Username + ";" + user.Password;
+            var token = Utility.Encrypt(user.Username + "~" + user.Password);
+            return token;
         }
 
         public User Decrypt(string token)
         {
+            token = Utility.Decrypt(token);
             var userRepository = new UserRepository();
-            string username = token.Split(';')[0];
-            string password = token.Split(';')[1];
+            string username = token.Split('~')[0];
+            string password = token.Split('~')[1];
             return userRepository.GetEntity(username, password);
         }
 
